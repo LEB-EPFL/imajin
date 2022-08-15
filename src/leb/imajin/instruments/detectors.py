1931f1155445aba131ce73ade76f05d568b1eeab
Kyle Douglass <kyle.douglass@epfl.ch>
2022-08-15 10:13:33 +0200

    Refactored CMOSCamera to validate inputs

--- src/leb/imajin/instruments/detectors.py
+++ src/leb/imajin/instruments/detectors.py
@@ -14,34 +14,69 @@ class BitDepth(Enum):
     THIRTYTWO = (32, np.uint32)
 
 
-class CMOSCamera:
+class SimpleCMOSCamera:
     def __init__(
         self,
+        baseline: int = 100,
+        bit_depth: BitDepth = BitDepth.TWELVE,
+        dark_noise: float = 2.29,
         qe: float = 0.69,
         sensitivity: float = 5.88,
-        dark_noise: float = 2.29,
-        bit_depth: BitDepth = BitDepth.TWELVE,
-        baseline: int = 100,
     ):
-        if qe < 0 or qe > 1:
-            raise ValueError("qe must be between 0 and 1, inclusive")
-
-        if sensitivity < 0:
-            raise ValueError("sensitivity must be greater than zero")
-
-        if dark_noise < 0:
-            raise ValueError("dark_noise must be greater than zero")
-
-        if baseline < 0:
-            raise ValueError("baseline must be greater than zero")
-
+        self.baseline = baseline
+        self.bit_depth = bit_depth
+        self.dark_noise = dark_noise
         self.qe = qe
         self.sensitivity = sensitivity
-        self.dark_noise = dark_noise
-        self.bit_depth = bit_depth
-        self.baseline = baseline
 
-    def snapshot(
+    def __repr__(self) -> str:
+        return f"SimpleCMOSCamera(baseline={self.baseline}, bit_depth={self.bit_depth}, dark_noise={self.dark_noise}, qe={self.qe}, sensitivity={self.sensitivity})"
+
+    @property
+    def baseline(self) -> int:
+        return self._baseline
+
+    @baseline.setter
+    def baseline(self, value: int) -> None:
+        if value < 0:
+            raise ValueError("baseline must be greater than zero")
+
+        self._baseline = value
+
+    @property
+    def dark_noise(self) -> float:
+        return self._dark_noise
+
+    @dark_noise.setter
+    def dark_noise(self, value: float) -> None:
+        if value < 0:
+            raise ValueError("dark_noise must be greater than zero")
+
+        self._dark_noise = value
+
+    @property
+    def qe(self) -> float:
+        return self._qe
+
+    @qe.setter
+    def qe(self, value: float) -> None:
+        if value < 0 or value > 1:
+            raise ValueError("qe must be between 0 and 1, inclusive")
+
+        self._qe = value
+
+    @property
+    def sensitivity(self) -> float:
+        return self._sensitivity
+
+    @sensitivity.setter
+    def sensitivity(self, value: float) -> None:
+        if value < 0:
+            raise ValueError("sensitivity must be greater than zero")
+
+        self._sensitivity = value
+
+    def get_image(
         self,
         photons: npt.NDArray[np.unsignedinteger],
         rs: Optional[RandomState] = None,
--- tests/unit/leb/imajin/instrument/test_detectors.py
+++ tests/unit/leb/imajin/instrument/test_detectors.py
@@ -1,30 +1,30 @@
 import numpy as np
 import pytest
 
-from leb.imajin.instruments import CMOSCamera
+from leb.imajin.instruments import SimpleCMOSCamera
 
 
 @pytest.mark.parametrize(
     "inputs",
     [
-        {"qe": -0.5, "sensitivity": 5.88, "dark_noise": 2.29, "baseline": 100},
-        {"qe": 1.5, "sensitivity": 5.88, "dark_noise": 2.29, "baseline": 100},
-        {"qe": 0.69, "sensitivity": -5.9, "dark_noise": 2.29, "baseline": 100},
-        {"qe": 0.69, "sensitivity": 5.88, "dark_noise": -2.3, "baseline": 100},
-        {"qe": 0.69, "sensitivity": 5.88, "dark_noise": 2.29, "baseline": -50},
+        {"baseline": 100, "dark_noise": 2.29, "qe": -0.5, "sensitivity": 5.88},
+        {"baseline": 100, "dark_noise": 2.29, "qe": 1.5, "sensitivity": 5.88},
+        {"baseline": 100, "dark_noise": 2.29, "qe": 0.69, "sensitivity": -5.9},
+        {"baseline": 100, "dark_noise": -2.3, "qe": 0.69, "sensitivity": 5.88},
+        {"baseline": -50, "dark_noise": 2.29, "qe": 0.69, "sensitivity": 5.88},
     ],
 )
 def test_CMOSCamera_bad_intrinsic_parameter_values(inputs):
     with pytest.raises(ValueError):
-        CMOSCamera(**inputs)
+        SimpleCMOSCamera(**inputs)
 
 
 def test_CMOSCamera_snapshot():
     num_pixels = (128, 128)
 
-    camera = CMOSCamera()
+    camera = SimpleCMOSCamera()
     photons = 1000 * np.ones(num_pixels)
 
-    img = camera.snapshot(photons)
+    img = camera.get_image(photons)
 
     assert img.shape == num_pixels
