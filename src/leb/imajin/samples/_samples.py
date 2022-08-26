72844aaa292b8316f212bd1e04657ae70167c43a
Kyle Douglass <kyle.douglass@epfl.ch>
2022-08-26 09:06:00 +0200

    Remove Response protocol in favor of specific responses

--- src/leb/imajin/samples/_samples.py
+++ src/leb/imajin/samples/_samples.py
@@ -1,52 +1,12 @@
 from dataclasses import dataclass
-from typing import Generic, Iterable, TypeVar
+from typing import Generic, List, TypeVar
 
 import numpy as np
 import numpy.typing as npt
 
-from leb.imajin import Response, Validation
 from leb.imajin.instruments import Source
 
-from ._sample import Sample
-
-T1 = TypeVar("T1", bound=npt.NBitBase)
-
-
-@dataclass(frozen=True)
-class EmitterToSource(Validation, Generic[T1]):
-    """Models the response of single fluorescent emitters to a radiation source."""
-
-    x: np.floating[T1]
-    y: np.floating[T1]
-    z: np.floating[T1]
-    photons: int
-    wavelength: float
-
-    def validate_photons(self, value: int, **_) -> int:
-        if value < 0:
-            raise ValueError("photons cannot be less than zero")
-        return value
-
-    def validate_wavelength(self, value: float, **_) -> float:
-        if value <= 0:
-            raise ValueError("wavelength must be greater than or equal to zero")
-        return value
-
-
-@dataclass(frozen=True)
-class EmittersToSource(Response):
-    """Models the response of a set of fluorescent emitters to a radiation source."""
-
-    emitters: Iterable[EmitterToSource]
-
-    def __post_init__(self):
-        object.__setattr__(self, "emitters", list(self.emitters))
-
-    def __iter__(self):
-        return iter(self.emitters)
-
-    def __len__(self):
-        return len(self.emitters)
+from ._sample import EmitterResponse, Sample
 
 
 class NullSample(Sample):
@@ -93,7 +53,7 @@ class ConstantEmitters(Sample):
 
         self._wavelength = value
 
-    def response(self, source: Source, dt: float) -> EmittersToSource:
+    def response(self, source: Source, dt: float) -> List[EmitterResponse]:
         """Returns the response of the emitters to the radiation source.
 
         ConstantEmitters emit a constant number of photons per unit time interval, regardless of
@@ -101,7 +61,7 @@ class ConstantEmitters(Sample):
 
         """
         photons = int(self.rate * dt)
-        return EmittersToSource(
-            EmitterToSource(x, y, z, photons, self.wavelength)
+        return [
+            EmitterResponse(x, y, z, photons, self.wavelength)
             for x, y, z in zip(self.x, self.y, self.z)
-        )
+        ]
