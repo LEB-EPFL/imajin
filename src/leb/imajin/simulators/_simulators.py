883be4c4ff3bb5e43c84abb591874203d0f70a56
Kyle Douglass <kyle.douglass@epfl.ch>
2022-09-23 08:47:31 +0200

    Add processors to Simulator class
    This enables hooking in processing routines to each simulation step.

--- src/leb/imajin/simulators/_simulators.py
+++ src/leb/imajin/simulators/_simulators.py
@@ -1,10 +1,41 @@
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional, Protocol, Sequence, Tuple
 
 import numpy as np
 from numpy import random
 
-from leb.imajin import Detector, DetectorResponse, Optics, Sample, Source, Validation
+from leb.imajin import (
+    Detector,
+    DetectorResponse,
+    Optics,
+    OpticsResponse,
+    Sample,
+    SampleResponse,
+    Source,
+    Validation,
+)
+
+
+class Processor(Protocol):
+    """A class for processing or modifying the simulation state at each time step.
+
+    A processor is a Callable that is called before and after each simulation step.
+
+    This may be useful for constructing reward functions, writing the state of the simulation to a
+    file, or modifying a simulation component value in response to the state, for example. Any
+    results should be stored within the Processor instance because the Callable does not return a
+    value.
+
+    """
+
+    def __call__(
+        self,
+        simulator: "Simulator",
+        detector_response: Optional[DetectorResponse] = None,
+        sample_response: Optional[SampleResponse] = None,
+        optics_response: Optional[OpticsResponse] = None,
+    ) -> None:
+        """Processes the state of the simulation."""
 
 
 @dataclass
@@ -15,14 +46,23 @@ class Simulator(Validation):
     source: Source
 
     time: float = 0
-    dt: float = 0.01
+    dt: float = 1.0
     x_lim: Tuple[int, int] = (0, 32)
     y_lim: Tuple[int, int] = (0, 32)
     num_measurements: int = 100
 
+    preprocessors: Optional[Sequence[Processor]] = None
+    post_processors: Optional[Sequence[Processor]] = None
+
     rng: Optional[random.Generator] = None
 
     def __post_init__(self):
+        if self.preprocessors is None:
+            self.preprocessors = []
+
+        if self.post_processors is None:
+            self.post_processors = []
+
         if self.rng is None:
             self.rng = random.default_rng()
 
@@ -32,11 +72,25 @@ class Simulator(Validation):
         return value
 
     def step(self) -> DetectorResponse:
+        assert self.preprocessors is not None
+        assert self.post_processors is not None
+
+        for processor in self.preprocessors:
+            processor(self)
+
         sample_response = self.sample.response(self.time, self.dt, self.source)
         optics_response = self.optics.response(self.x_lim, self.y_lim, sample_response)
         detector_response = self.detector.response(optics_response, rng=self.rng)
 
         self.time += self.dt
+
+        for processor in self.post_processors:
+            processor(
+                self,
+                detector_response=detector_response,
+                sample_response=sample_response,
+                optics_response=optics_response,
+            )
 
         return detector_response
 
